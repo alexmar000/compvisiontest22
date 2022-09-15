c502242f5ef58661a405e6bbb279c858e696ca91
Alex Olave <alex.olave@inteheltfel.se>
2022-09-15 13:34:44 +0200

    init

---
+++ AzureComputerVisionTest22/ImageAnalysisService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureComputerVisionTest22
+{
+    internal class ImageAnalysisService
+    {
+        private static readonly string ApiKey = ConfigurationManager.AppSettings.Get("CogSvcApiKey")!;
+        private static readonly string Endpoint = ConfigurationManager.AppSettings["CogSvcEndpoint"]!;
+        private string sampleDog = "https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg";
+        ComputerVisionClient client;
+        public ImageAnalysisService()
+        {
+            client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(ApiKey))
+            { Endpoint = Endpoint };
+        }
+
+        public async Task AnalyzeImage(string url)
+        {
+            var features = new List<VisualFeatureTypes?>()
+            { VisualFeatureTypes.Description };
+            Console.Clear();
+            Console.WriteLine("Fetching results...");
+
+            ImageAnalysis results = await client.AnalyzeImageAsync(url, visualFeatures: features);
+            Console.Clear();
+
+            foreach (var description in results.Description.Captions)
+            {
+                Console.WriteLine($"I think this is {description.Text} with confidence {description.Confidence}");
+            }
+        }
+    }
+}
---
+++ AzureComputerVisionTest22/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AzureComputerVisionTest22;
+
+class Program
+{
+    static async Task Main()
+    {
+        var client = new ImageAnalysisService();
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Enter image url to analyze:");
+            await client.AnalyzeImage(Console.ReadLine());
+            Console.WriteLine("Type 'r' to try again");
+        }
+        while (Console.ReadLine() == "r");
+    }
+}
